c383d160579021acb1f8c0eea62ea9b99ed65c00
dev-vishalg <gvishal647@gmail.com>
2024-08-08 13:02:50 +0530

    added all code

---
+++ src/main/java/com/medicine/MedicineBooking/MedicineBookingApplication.java
@@ -0,0 +1,13 @@
+package com.medicine.MedicineBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineBookingApplication.class, args);
+	}
+
+}
